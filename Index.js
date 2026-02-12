abce41f640370b86fddccfe94d7b9f3bdf68d03f
pongnakorn3 <pongnakorn3@gmail.com>
2026-02-12 15:11:50 +0700

    Index Test

--- Index.js
+++ Index.js
@@ -1,1 +1,7 @@
-
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASS,
+  port: process.env.DB_PORT,
+});
