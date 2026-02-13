ff79241cd910a3e85538a6121eafbac02183dfcc
FairyKung <cafefc123456@gmail.com>
2026-02-13 14:54:31 +0700

    Test reg

--- Index.js
+++ Index.js
@@ -1,12 +1,17 @@
 import 'dotenv/config';
 import express from 'express';
 import pg from 'pg';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+//import pool from './pool.js';
+
 
 const { Pool } = pg;
 
 const app = express();
 const PORT = 3000;
-
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 const pool = new Pool({
   user: process.env.DB_USER,
   host: process.env.DB_HOST,
@@ -28,5 +33,46 @@ app.listen(PORT, async () => {
   } catch (err) {
     console.error('❌ Database connection failed');
     console.error(err);
+    
+  }
+});
+app.post('/register-owner', async (req, res) => {
+  try {
+    const { full_name, email, password } = req.body;
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const result = await pool.query(
+      `INSERT INTO users (full_name, email, password, role)
+       VALUES ($1, $2, $3, 'owner')
+       RETURNING id, full_name, email`,
+      [full_name, email, hashedPassword]
+    );
+
+    res.status(201).json(result.rows[0]);
+
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Registration failed' });
+  }
+});
+app.post('/register-owner', async (req, res) => {
+  try {
+    const { full_name, email, password } = req.body;
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const result = await pool.query(
+      `INSERT INTO users (full_name, email, password, role)
+       VALUES ($1, $2, $3, 'owner')
+       RETURNING id, full_name, email`,
+      [full_name, email, hashedPassword]
+    );
+
+    res.status(201).json(result.rows[0]);
+
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Registration failed' });
   }
 });
