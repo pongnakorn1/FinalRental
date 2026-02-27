b966d343a8d828e9e3021fcd525e11330332d858
pongnakorn3 <pongnakorn3@gmail.com>
2026-02-28 01:50:46 +0700

    Fix LINE Login: update to passport-line-v2 and add email scope

--- config/passport.js
+++ config/passport.js
@@ -22,7 +22,7 @@ passport.use(new FacebookStrategy({
     clientID: process.env.FACEBOOK_APP_ID,
     clientSecret: process.env.FACEBOOK_APP_SECRET,
     callbackURL: process.env.FACEBOOK_CALLBACK_URL,
-    profileFields: ['id', 'displayName', 'emails'] // ขอข้อมูลที่จำเป็น
+    profileFields: ['id', 'displayName'] // ขอข้อมูลที่จำเป็น
   },
   async (accessToken, refreshToken, profile, done) => {
     return done(null, profile);
